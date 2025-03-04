013aa0ca0c0e48efc09b609fb12fe57d701ccd22
Avinash8577rai <avinashkumarrai8577@gmail.com>
2025-03-04 10:19:56 +0530

    [Avinash]Added ability to search contacts by city or state using filter()

--- src/Index.js
+++ src/Index.js
@@ -5,24 +5,31 @@ const addressBook = new AddressBook();
 
 try {
     const contact1 = new Contact(
-        "Avinash", "Kumar", "Bhopal", "India", "MadhyaPradesh", "100001", "9696439931", "avinashkumarrai8577@gmail.com"
-    );
-    console.log(addressBook.addContact(contact1));
-    const contact2 = new Contact(
-        "Ayush", "Rai", "Bhopal", "India","Chattisgarh","100000","9197979988","ayushirai@gmail.com"
-    );
-    console.log(addressBook.addContact(contact2)); 
-    console.log("All Contacts Before editing", addressBook.getAllContacts());
-    console.log(addressBook.editContact("Ayush", { address: "Bhopal", city: "New York", phone: "9234567890" }));
-    console.log("All Contacts After editing", addressBook.getAllContacts());
-    console.log(addressBook.deleteContact("Ayush"));
-    console.log("All Contacts After Deleting:", addressBook.getAllContacts());
-    console.log("Total Contacts:", addressBook.countContacts());
-    const duplicateContact = new Contact(
          "Avinash", "Kumar", "Bhopal", "India", "MadhyaPradesh", "100001", "9696439931", "avinashkumarrai8577@gmail.com"
     );
-    console.log(addressBook.addContact(duplicateContact)); 
+    console.log(addressBook.addContact(contact1)); 
+
+    const contact2 = new Contact(
+         "Ayush", "Rai", "Bhopal", "India","Chattisgarh","100000","9197979988","ayushirai@gmail.com"
+    )
+    console.log(addressBook.addContact(contact2)); 
+    console.log("All Contacts Before editing", addressBook.getAllContacts());
+    console.log(addressBook.editContact("Rakesh", { address: "Pipariya", city: "New York", phone: "9234567890" }));
+    console.log("All Contacts After editing", addressBook.getAllContacts());
+    console.log(addressBook.deleteContact("Rakesh"));
+    console.log("All Contacts After Deleting:", addressBook.getAllContacts());
+    console.log("Total Contacts:", addressBook.countContacts()); 
+
+    const duplicateContact = new Contact(
+          "Avinash", "Kumar", "Bhopal", "India", "MadhyaPradesh", "100001", "9696439931", "avinashkumarrai8577@gmail.com"
+    );
+    console.log(addressBook.addContact(duplicateContact));  
+
+    console.log("Contacts in India:");
+    console.log(addressBook.searchByCityOrState("MadhyaPradesh"));
+
 } catch (error) {
     console.error("Error:", error.message);
 }
+
 
